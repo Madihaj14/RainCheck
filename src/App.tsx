555fd8e32d7c4430a110602a6b9675046cf28651
Madihaj14 <madihajk14@gmail.com>
2025-03-21 09:33:55 +0530

    installed shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  
-
   return (
-   <div>This will be my first self made project!</div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
