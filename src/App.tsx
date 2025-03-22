aedbe81cb2358f99bbd25978d5d70e6bb33efdaa
Madihaj14 <madihajk14@gmail.com>
2025-03-22 09:31:13 +0530

    adding dark mode and chaning some background settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css"
 import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
 // Removed unused Button import
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>Hello</Layout>
+      <ThemeProvider defaultTheme="dark"> 
+        
+        <Layout>Hello</Layout> 
+      
+      </ThemeProvider>
+      
 
     </BrowserRouter>
   )
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,8 +7,8 @@ const layout = ({ children }: PropsWithChildren) => {
     <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
     </main>
-    <footer className='border-t backdrop-blur'>
-        <div className='container mx-auto px-4 text-center text-black-200'>
+    <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
+        <div className='container mx-auto px-4 text-center text-gray-400'>
             <p>Made with ❤️ by Madiha</p>
         </div>
     </footer>
